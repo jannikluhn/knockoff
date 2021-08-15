6e91267f962b5951b6df4029d45eb83be2e544e4
Jannik Luhn <jannik@brainbot.com>
2021-08-15 23:06:30 +0200

    Setup tailwind

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -3,6 +3,18 @@
     <SelectNFTForm @submit="nftTokenLocationID = $event" />
     <NFTView :nftTokenLocationID="nftTokenLocationID" />
     <Gallery />
+
+    <div
+      class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
+    >
+      <div class="flex-shrink-0">
+        <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
   </div>
 </template>
 
