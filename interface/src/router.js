915671783e0a30dd244372c00a864a3ffb719795
Jannik Luhn <jannik@brainbot.com>
2021-08-16 19:44:11 +0200

    Add 404 Not Found page

--- interface/src/router.js
+++ interface/src/router.js
@@ -5,6 +5,7 @@ import LandingPage from "./pages/LandingPage.vue";
 import GalleryPage from "./pages/GalleryPage.vue";
 import OriginalPage from "./pages/OriginalPage.vue";
 import KnockoffPage from "./pages/KnockoffPage.vue";
+import NotFoundPage from "./pages/NotFoundPage.vue";
 
 function setupRouter() {
   Vue.use(VueRouter);
@@ -28,6 +29,10 @@ const routes = [
     path: "/knockoff/:chain/:contractAddress/:tokenID",
     component: KnockoffPage,
     props: true,
+  },
+  {
+    path: "*",
+    component: NotFoundPage,
   },
 ];
 
