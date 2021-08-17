30300998357a5b9f312a6504e8cdb1f77fd17485
Jannik Luhn <jannik@brainbot.com>
2021-08-17 23:11:24 +0200

    Don't setup vue-apollo

--- interface/src/main.js
+++ interface/src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 
 import { setupFormulate } from "./formulate.js";
 import { setupVuex, store } from "./vuex.js";
-import { setupApollo, apolloProvider } from "./apollo.js";
 import { setupRouter, router } from "./router";
 
 import "tailwindcss/tailwind.css";
@@ -11,12 +10,10 @@ import "tailwindcss/tailwind.css";
 Vue.config.productionTip = false;
 setupFormulate();
 setupVuex();
-setupApollo();
 setupRouter();
 
 new Vue({
   render: (h) => h(App),
   store,
-  apolloProvider,
   router,
 }).$mount("#app");
