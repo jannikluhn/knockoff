41dba04afa2dc23ca269815a8cc7ef85569d1161
Jannik Luhn <jannik@brainbot.com>
2021-08-19 18:44:21 +0200

    Add custom errors

--- interface/src/errors.js
+++ interface/src/errors.js
@@ -3,3 +3,27 @@ export default {
   BAD_STORE: "BAD_STORE",
   NETWORK_ERROR: "NETWORK_ERROR",
 };
+
+const errorCodes = {
+  UNEXPECTED_ERROR: "UNEXPECTED_ERROR",
+  NETWORK_ERROR: "NETWORK_ERROR",
+  CHAIN_ERROR: "CHAIN_ERROR",
+  APOLLO_ERROR: "APOLLO_ERROR",
+};
+
+class KnockOffError extends Error {
+  constructor(code, message, obj, ...params) {
+    super(message, ...params);
+
+    this.code = code;
+    this.obj = obj || null;
+  }
+}
+
+function throwError(code, message, obj, ...params) {
+  const error = new KnockOffError(code, message, obj, ...params);
+  console.error(error);
+  throw error;
+}
+
+export { errorCodes, KnockOffError, throwError };
