cfb5f219d18cf8b5dc93583e3829d352350ef949
Jannik Luhn <jannik@brainbot.com>
2021-08-22 18:54:34 +0200

    Add original view

---
+++ interface/src/components/OriginalView.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <p>{{ contractState }}</p>
+    <p>{{ tokenState }}</p>
+    <p>{{ metadata }}</p>
+  </div>
+</template>
+
+<script>
+import {
+  fetchOriginalContractState,
+  fetchOriginalTokenState,
+} from "../originalFetching";
+import { fetchERC721Metadata } from "../erc721MetadataFetching.js";
+
+export default {
+  name: "OriginalView",
+  props: ["chainID", "contractAddress", "tokenID"],
+
+  data() {
+    return {
+      requestCounter: 0,
+
+      contractState: null,
+      tokenState: null,
+      metadata: null,
+    };
+  },
+
+  computed: {
+    tokenInputProps() {
+      if (!this.chainID || !this.contractAddress || !this.tokenID) {
+        return null;
+      }
+      return [this.chainID, this.contractAddress, this.tokenID];
+    },
+  },
+
+  watch: {
+    tokenInputProps: {
+      immediate: true,
+      handler() {
+        this.tokenChangeHandler();
+      },
+    },
+  },
+
+  methods: {
+    tokenChangeHandler() {
+      if (!this.tokenInputProps) {
+        this.token = null;
+        this.metadata = null;
+      } else {
+        this.fetch();
+      }
+    },
+
+    async fetch() {
+      const requestID = this.requestCounter;
+      this.currentRequest = requestID;
+      this.requestCounter += 1;
+
+      this.contractState = null;
+      this.tokenState = null;
+      this.metadata = null;
+
+      try {
+        const contractState = await fetchOriginalContractState(
+          this.chainID,
+          this.contractAddress
+        );
+        if (this.currentRequest !== requestID) {
+          return;
+        }
+        this.contractState = contractState;
+
+        const tokenState = await fetchOriginalTokenState(
+          this.chainID,
+          this.contractAddress,
+          this.tokenID,
+          this.contractState
+        );
+        if (this.currentRequest !== requestID) {
+          return;
+        }
+        this.tokenState = tokenState;
+
+        if (this.contractState.supportsIERC721 && this.tokenState.exists) {
+          const metadata = await fetchERC721Metadata(
+            this.chainID,
+            this.contractAddress,
+            this.tokenID
+          );
+          if (this.currentRequest !== requestID) {
+            return;
+          }
+          this.metadata = metadata;
+          console.log(this.metadata);
+        }
+      } finally {
+        if (this.currentRequest === requestID) {
+          this.currentRequest = null;
+        }
+      }
+    },
+  },
+};
+</script>
