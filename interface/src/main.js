510fca90077b3f3b464af6d6bc1156a2e7c5e802
Jannik Luhn <jannik@brainbot.com>
2021-08-22 16:18:33 +0200

    Remove vuex

--- interface/src/main.js
+++ interface/src/main.js
@@ -2,18 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import { setupFormulate } from "./formulate.js";
-import { setupVuex, store } from "./vuex.js";
 import { setupRouter, router } from "./router";
 
 import "tailwindcss/tailwind.css";
 
 Vue.config.productionTip = false;
 setupFormulate();
-setupVuex();
 setupRouter();
 
 new Vue({
   render: (h) => h(App),
-  store,
   router,
 }).$mount("#app");
