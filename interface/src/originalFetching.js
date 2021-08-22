920307e88937520c5d72556520f506b0f3f3f5d1
Jannik Luhn <jannik@brainbot.com>
2021-08-22 18:52:11 +0200

    Fix original fetching

--- interface/src/originalFetching.js
+++ interface/src/originalFetching.js
@@ -17,8 +17,8 @@ async function fetchOriginalContractState(chainID, contractAddress) {
   const provider = providers[chainID];
   if (provider === undefined) {
     throwError(
-      "no provider for chain with id " + chainID,
-      errorCodes.UNEXPECTED_ERROR
+      errorCodes.UNEXPECTED_ERROR,
+      "no provider for chain with id " + chainID
     );
   }
 
@@ -33,7 +33,7 @@ async function fetchOriginalContractState(chainID, contractAddress) {
       e
     );
   }
-  if (code == "0x") {
+  if (code === "0x") {
     state.hasCode = false;
     return state;
   }
@@ -89,7 +89,7 @@ async function fetchOriginalTokenState(
   chainID,
   contractAddress,
   tokenID,
-  nftContractState
+  originalContractState
 ) {
   let state = makeNFTTokenState();
 
@@ -101,33 +101,30 @@ async function fetchOriginalTokenState(
     );
   }
 
-  if (!nftContractState.hasCode) {
+  if (!originalContractState.hasCode) {
     return state;
   }
 
-  if (nftContractState.supportsIERC721) {
+  if (originalContractState.supportsIERC721) {
     const attachedContract = contractFactories["IERC721"].attach(
       contractAddress
     );
     const contract = attachedContract.connect(provider);
 
-    let owner;
-    let exists;
     try {
-      owner = await contract.ownerOf(tokenID);
-      exists = true;
+      state.owner = await contract.ownerOf(tokenID);
+      state.exists = true;
     } catch (e) {
       if (e.code === ethers.errors.CALL_EXCEPTION) {
-        exists = false;
+        state.owner = null;
+        state.exists = false;
       } else {
         throwError(errorCodes.CHAIN_ERROR, "failed to query token owner", e);
       }
-      state.owner = owner;
-      state.exists = exists;
     }
   }
 
-  if (nftContractState.supportsIERC721Metadata) {
+  if (state.exists && originalContractState.supportsIERC721Metadata) {
     const attachedContract = contractFactories["IERC721Metadata"].attach(
       contractAddress
     );
