cdb55894ad6378ef042f1cb008519a72f9b4e67d
Jannik Luhn <jannik@brainbot.com>
2021-08-22 19:47:23 +0200

    Run prettier

--- interface/src/pages/KnockoffPage.vue
+++ interface/src/pages/KnockoffPage.vue
@@ -1,13 +1,13 @@
 <template>
-<div class="flex flex-col items-center py-8 xl:flex-row-reverse">
-  <Artwork />
-  <div class="flex flex-col items-center xl:w-1/2 xl:px-3">
-    <Header />
-    <NFTDataTable />
-    <Button :isPrimary="true" message="knock-off"/>
-    <Button :isPrimary="false" message="view original"/>
+  <div class="flex flex-col items-center py-8 xl:flex-row-reverse">
+    <Artwork />
+    <div class="flex flex-col items-center xl:w-1/2 xl:px-3">
+      <Header />
+      <NFTDataTable />
+      <Button :isPrimary="true" message="knock-off" />
+      <Button :isPrimary="false" message="view original" />
+    </div>
   </div>
-</div>
 </template>
 
 <script>
