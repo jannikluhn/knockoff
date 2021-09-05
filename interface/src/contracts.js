7c92956191e59741383f6cf4384462a6c8318d6c
Jannik Luhn <jannik@brainbot.com>
2021-09-05 21:07:23 +0200

    Add KnockOff contract ABI

--- interface/src/contracts.js
+++ interface/src/contracts.js
@@ -2,6 +2,7 @@ import { ethers } from "ethers";
 import IERC165ABI from "./assets/IERC165ABI.json";
 import IERC721ABI from "./assets/IERC721ABI.json";
 import IERC721MetadataABI from "./assets/IERC721MetadataABI.json";
+import ERC721KnockOffsABI from "./assets/ERC721KnockOffsABI.json";
 
 const interfaceIDs = {
   IERC165: "0x01ffc9a7",
@@ -14,6 +15,7 @@ const contractFactories = {
   IERC165: new ethers.ContractFactory(IERC165ABI, ""),
   IERC721: new ethers.ContractFactory(IERC721ABI, ""),
   IERC721Metadata: new ethers.ContractFactory(IERC721MetadataABI, ""),
+  ERC721KnockOffs: new ethers.ContractFactory(ERC721KnockOffsABI, ""),
 };
 
 export { interfaceIDs, contractFactories };
