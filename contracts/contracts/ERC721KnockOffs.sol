54b83eef5bc60dc8b77d031aee9c7682b501a540
Jannik Luhn <jannik@brainbot.com>
2021-09-25 13:00:58 +0200

    Make knockoff contract ownable

--- contracts/contracts/ERC721KnockOffs.sol
+++ contracts/contracts/ERC721KnockOffs.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.6;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -13,7 +14,7 @@ struct Original {
     uint64 numKnockOffs;
 }
 
-contract ERC721KnockOffs is ERC721Enumerable {
+contract ERC721KnockOffs is ERC721Enumerable, Ownable {
     // TODO: add minter
     event Minted(
         IERC721Metadata originalContract,
@@ -28,7 +29,7 @@ contract ERC721KnockOffs is ERC721Enumerable {
     mapping(uint256 => Original) public originals; // original id => Original
     mapping(uint256 => uint64) public serialNumbers; // token id => serial number
 
-    constructor() ERC721("KnockOffs", "KNO") {}
+    constructor() ERC721("KnockOffs", "KNO") Ownable() {}
 
     function mint(
         address receiver,
