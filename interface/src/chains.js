cce835e5ad256e3ea2b19825cdb11313ac25fd1c
Jannik Luhn <jannik@brainbot.com>
2021-09-26 23:17:59 +0200

    Remove testnets

--- interface/src/chains.js
+++ interface/src/chains.js
@@ -11,25 +11,6 @@ export const chains = {
     graphUrl:
       "https://api.thegraph.com/subgraphs/name/jannikluhn/knockoffs-mainnet",
   },
-  4: {
-    name: "Rinkeby",
-    pathSegment: "rinkeby",
-    chainID: 4,
-    erc721KnockOffsAddress: "0xDb2E7973D8e4d3ed5052f06d43462C41aEE6DF76",
-    providerUrl:
-      "https://rinkeby.infura.io/v3/cb47771bf3324acc895994de6752654b",
-    graphUrl:
-      "https://api.thegraph.com/subgraphs/name/jannikluhn/knockoffs-rinkeby",
-  },
-  5: {
-    name: "Goerli",
-    pathSegment: "goerli",
-    chainID: 5,
-    erc721KnockOffsAddress: "0x5576f07713D3aBEd1677Ad50AF90a461364FA42d",
-    providerUrl: "https://goerli.infura.io/v3/cb47771bf3324acc895994de6752654b",
-    graphUrl:
-      "https://api.thegraph.com/subgraphs/name/jannikluhn/knockoffs-goerli",
-  },
   100: {
     name: "XDai",
     pathSegment: "xdai",
