6b1bbdc4ff826f367c29ded88f7308279e054578
Jannik Luhn <jannik@brainbot.com>
2021-09-26 22:28:53 +0200

    Add footer

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex flex-col h-screen justify-between">
     <NavBar />
-    <div class="container mx-auto px-2">
+    <div class="container mx-auto px-2 flex-grow">
       <router-view></router-view>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     NavBar,
+    Footer,
   },
 
   data() {
---
+++ interface/src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="border-t-2 border-gray-100 relative z-50">
+    <div class="px-4 flex justify-end">
+      <a
+        class="h-full py-4 px-4 cursor-pointer font-medium"
+        href="https://medium.com"
+      >
+        Blog
+      </a>
+      <a
+        class="h-full py-4 px-4 cursor-pointer font-medium"
+        href="https://medium.com"
+      >
+        Github
+      </a>
+      <a
+        class="h-full py-4 px-4 cursor-pointer font-medium"
+        href="https://medium.com"
+      >
+        Twitter
+      </a>
+      <a
+        class="h-full py-4 px-4 cursor-pointer font-medium"
+        href="https://medium.com"
+      >
+        Discord
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
