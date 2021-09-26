4a7128f56d95fd165dc535c3c7c4fc179f36adcf
Jannik Luhn <jannik@brainbot.com>
2021-09-26 22:06:31 +0200

    Deployment script

---
+++ interface/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'www.knockoff.lol' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:jannikluhn/knockoff.git main:gh-pages
+
+cd -
