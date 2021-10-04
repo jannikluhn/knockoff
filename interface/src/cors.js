5f21e4f33d3038562a45e66202761f5613fdde96
Jannik Luhn <jannik@brainbot.com>
2021-10-04 18:41:40 +0200

    Don't route ethlings through corsproxy
    For some reason, the request gives a 400 response when performed through
    the corsproxy. It works without it.

--- interface/src/cors.js
+++ interface/src/cors.js
@@ -1,5 +1,12 @@
-export const corsProxy = "https://cors.knockoff.lol/";
+const corsProxy = "https://cors.knockoff.lol/";
+const corsWhitelist = {
+  "api.ethlings.com": true,
+};
 
 export function getCORSProxyURL(url) {
+  const urlObj = new URL(url);
+  if (corsWhitelist[urlObj.host]) {
+    return url;
+  }
   return corsProxy + url;
 }
